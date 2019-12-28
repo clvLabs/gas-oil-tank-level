b589291d6cc1789f4c5b25de5a2f744bc4727464
Tony Aguilar <onixag@gmail.com>
2019-12-28 01:39:31 +0100

    Center texts / use different fonts

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@
 #define DISPLAY_HEIGHT      ( 64 )
 #define DISPLAY_MINX        ( 0 )
 #define DISPLAY_MINY        ( 0 )
-#define DISPLAY_MAXX        ( DISPLAY_WIDTH-1 )
-#define DISPLAY_MAXY        ( DISPLAY_HEIGHT-1 )
+#define DISPLAY_MAXX        ( DISPLAY_MINX + DISPLAY_WIDTH - 1 )
+#define DISPLAY_MAXY        ( DISPLAY_MINY + DISPLAY_HEIGHT - 1 )
 
 // Level calibration
 #define DIST2FLOOR_CM   ( 120 )
@@ -31,35 +31,42 @@ void clearScreen()
   do {} while( u8g.nextPage() );
 }
 
-void updateUI(long cm)
-{
+void drawStrCentered(const char *str, u8g_uint_t y) {
+  u8g_uint_t strWidth = u8g.getStrWidth(str);
+  u8g_uint_t x = DISPLAY_MINX + ( (DISPLAY_WIDTH - strWidth) / 2 );
+
+  u8g.drawStr( x, y, str);
+}
+
+void updateUI(long cm) {
   long distFromBottom = BOTTOM_CM - cm;
-  char lcLine1[20];
-  char lcLine2[20];
+  char lcDistanceStr[20];
+  char lcPctStr[20];
 
 
   if (distFromBottom < 1) {
-    sprintf( lcLine1, "%ld cm", cm );
-    sprintf( lcLine2, "+%ld!!", (distFromBottom*-1) );
+    sprintf( lcDistanceStr, "%ld cm", cm );
+    sprintf( lcPctStr, "+%ld!!", (distFromBottom*-1) );
   } else {
-    sprintf( lcLine1, "%ld cm", cm );
+    sprintf( lcDistanceStr, "%ld cm", cm );
 
     // Compensate lack of float
     long fillFactor = (distFromBottom * 1000) / TANK_HEIGHT_CM;
     int percent = fillFactor / 10;
     int percentDecimal = fillFactor % 10;
-    sprintf( lcLine2, "%d.%d%%", percent, percentDecimal );
+    sprintf( lcPctStr, "%d.%d%%", percent, percentDecimal );
   }
 
-  Serial.print(lcLine1);
+  Serial.println(lcPctStr);
   Serial.print(" - ");
-  Serial.println(lcLine2);
+  Serial.print(lcDistanceStr);
 
   u8g.firstPage();
   do {
-    u8g.setFont(u8g_font_gdr25);
-    u8g.drawStr( DISPLAY_MINX, 32, lcLine1);
-    u8g.drawStr( DISPLAY_MINX, 32 + (DISPLAY_HEIGHT/2), lcLine2);
+    u8g.setFont(u8g_font_gdr30);
+    drawStrCentered( lcPctStr, 32 );
+    u8g.setFont(u8g_font_gdr12);
+    drawStrCentered( lcDistanceStr, 32 + (DISPLAY_HEIGHT/2) );
   } while( u8g.nextPage() );
 }
 
